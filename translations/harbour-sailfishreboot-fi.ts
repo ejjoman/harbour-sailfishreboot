e68f3d634726c5addb58ba8ba446be7d540ca172
tathhu <tathhu@users.noreply.github.com>
2015-04-16 03:40:24 +0300

    Finnish translations
    Hopefully it's complete! :D

--- translations/harbour-sailfishreboot-fi.ts
+++ translations/harbour-sailfishreboot-fi.ts
@@ -6,34 +6,34 @@
     <message>
         <location filename="../qml/common/AboutModel.qml" line="9"/>
         <source>Project Home</source>
-        <translation type="unfinished"></translation>
+        <translation type="Projektin koti"></translation>
     </message>
     <message>
         <location filename="../qml/common/AboutModel.qml" line="10"/>
         <source>on GitHub</source>
-        <translation type="unfinished"></translation>
+        <translation type="Githubissa"></translation>
     </message>
     <message>
         <location filename="../qml/common/AboutModel.qml" line="11"/>
         <location filename="../qml/common/AboutModel.qml" line="17"/>
         <location filename="../qml/common/AboutModel.qml" line="23"/>
         <source>Project</source>
-        <translation type="unfinished"></translation>
+        <translation type="Projekti"></translation>
     </message>
     <message>
         <location filename="../qml/common/AboutModel.qml" line="15"/>
         <source>License</source>
-        <translation type="unfinished"></translation>
+        <translation type="Lisenssi"></translation>
     </message>
     <message>
         <location filename="../qml/common/AboutModel.qml" line="21"/>
         <source>Report a bug or send a feature request</source>
-        <translation type="unfinished"></translation>
+        <translation type="Ilmoita bugista - tai lähetä kehitysidea"></translation>
     </message>
     <message>
         <location filename="../qml/common/AboutModel.qml" line="22"/>
         <source>on GitHub Issues</source>
-        <translation type="unfinished"></translation>
+        <translation type="Githubissa"></translation>
     </message>
     <message>
         <location filename="../qml/common/AboutModel.qml" line="27"/>
@@ -42,50 +42,50 @@
         <location filename="../qml/common/AboutModel.qml" line="44"/>
         <location filename="../qml/common/AboutModel.qml" line="50"/>
         <source>Author</source>
-        <translation type="unfinished"></translation>
+        <translation type="Kehittäjä"></translation>
     </message>
     <message>
         <location filename="../qml/common/AboutModel.qml" line="36"/>
         <source>Twitter</source>
-        <translation type="unfinished"></translation>
+        <translation type="Twitter"></translation>
     </message>
     <message>
         <location filename="../qml/common/AboutModel.qml" line="42"/>
         <location filename="../qml/common/AboutModel.qml" line="48"/>
         <source>Donate</source>
-        <translation type="unfinished"></translation>
+        <translation type="Lahjoita"></translation>
     </message>
     <message>
         <location filename="../qml/common/AboutModel.qml" line="43"/>
         <source>via PayPal</source>
-        <translation type="unfinished"></translation>
+        <translation type="Paypalin kautta"></translation>
     </message>
     <message>
         <location filename="../qml/common/AboutModel.qml" line="49"/>
         <source>via Flattr</source>
-        <translation type="unfinished"></translation>
+        <translation type="Flattrin kautta"></translation>
     </message>
     <message>
         <location filename="../qml/common/AboutModel.qml" line="54"/>
         <source>Translation provided by</source>
-        <translation type="unfinished"></translation>
+        <translation type="Kääntäjä/t"></translation>
     </message>
     <message>
         <location filename="../qml/common/AboutModel.qml" line="57"/>
         <source>names-of-translators</source>
         <extracomment>Insert the Name(s) of the translator(s) here</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="Tatu Hammarén @tathhu"></translation>
     </message>
     <message>
         <location filename="../qml/common/AboutModel.qml" line="58"/>
         <location filename="../qml/common/AboutModel.qml" line="63"/>
         <source>Translation</source>
-        <translation type="unfinished"></translation>
+        <translation type="Käännökset"></translation>
     </message>
     <message>
         <location filename="../qml/common/AboutModel.qml" line="61"/>
         <source>Translate this app</source>
-        <translation type="unfinished"></translation>
+        <translation type="Käännä tämä sovellus"></translation>
     </message>
     <message>
         <location filename="../qml/common/AboutModel.qml" line="62"/>
@@ -98,22 +98,22 @@
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="16"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation type="Asetukset"></translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="26"/>
         <source>About SailfishReboot</source>
-        <translation type="unfinished"></translation>
+        <translation type="Tietoja SailfishRebootista"></translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="46"/>
         <source>Link will open</source>
-        <translation type="unfinished"></translation>
+        <translation type="Linkki avautuu"></translation>
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="49"/>
         <source>Mail app will open</source>
-        <translation type="unfinished"></translation>
+        <translation type="Sähköpostisovellus avautuu"></translation>
     </message>
 </context>
 <context>
@@ -121,7 +121,7 @@
     <message>
         <location filename="../qml/cover/CoverPage.qml" line="58"/>
         <source>Uptime</source>
-        <translation type="unfinished"></translation>
+        <translation type="Päälläoloaika"></translation>
     </message>
 </context>
 <context>
@@ -129,7 +129,7 @@
     <message>
         <location filename="../qml/pages/LicensePage.qml" line="21"/>
         <source>License</source>
-        <translation type="unfinished"></translation>
+        <translation type="Lisenssi"></translation>
     </message>
 </context>
 <context>
@@ -137,17 +137,17 @@
     <message>
         <location filename="../qml/pages/MainPage.qml" line="59"/>
         <source>Reboot</source>
-        <translation type="unfinished"></translation>
+        <translation type="Käynnistä uudelleen"></translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="76"/>
         <source>Shutdown</source>
-        <translation type="unfinished"></translation>
+        <translation type="Sammuta"></translation>
     </message>
     <message>
         <location filename="../qml/pages/MainPage.qml" line="112"/>
         <source>Uptime</source>
-        <translation type="unfinished"></translation>
+        <translation type="Ilman uudelleenkäynnistystä"></translation>
     </message>
 </context>
 <context>
@@ -155,32 +155,32 @@
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="16"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation type="Asetukset"></translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="20"/>
         <source>Custom colors</source>
-        <translation type="unfinished"></translation>
+        <translation type="Eri värit"></translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="21"/>
         <source>Use custom colors for reboot and shutdown actions</source>
-        <translation type="unfinished"></translation>
+        <translation type="Käytä eri värejä uudelleenkäynnistykselle ja sammutukselle"></translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="30"/>
         <source>Remorse timeout</source>
-        <translation type="unfinished"></translation>
+        <translation type="Katumusaika"></translation>
     </message>
     <message>
         <location filename="../qml/pages/SettingsPage.qml" line="40"/>
         <source>Off</source>
-        <translation type="unfinished"></translation>
+        <translation type="Pois"></translation>
     </message>
     <message numerus="yes">
         <location filename="../qml/pages/SettingsPage.qml" line="42"/>
         <source>%n second(s)</source>
-        <translation type="unfinished">
+        <translation type="%n sekunti(a)">
             <numerusform></numerusform>
         </translation>
     </message>
@@ -190,12 +190,12 @@
     <message>
         <location filename="../qml/harbour-sailfishreboot.qml" line="57"/>
         <source>Your device will shutdown</source>
-        <translation type="unfinished"></translation>
+        <translation type="Laitteesi sulkeutuu"></translation>
     </message>
     <message>
         <location filename="../qml/harbour-sailfishreboot.qml" line="63"/>
         <source>Your device will reboot</source>
-        <translation type="unfinished"></translation>
+        <translation type="Laitteesi käynnistyy uudelleen"></translation>
     </message>
 </context>
 </TS>
