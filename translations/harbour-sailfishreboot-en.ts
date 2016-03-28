b4e62e57b24589ad3ba9e9e107d71caa42d58e74
Michael Neufing <michael@neufing.org>
2016-03-28 13:51:15 +0200

    English ts file simplified/reformatted by Linguist

--- translations/harbour-sailfishreboot-en.ts
+++ translations/harbour-sailfishreboot-en.ts
@@ -1,82 +1,82 @@
-<?xml version='1.0' encoding='utf-8'?>
+<?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="en">
 <context>
     <name>AboutModel</name>
     <message>
-        <location filename="../qml/common/AboutModel.qml" line="10"></location>
+        <location filename="../qml/common/AboutModel.qml" line="10"/>
         <source>Project Home</source>
         <translation>Project Home</translation>
     </message>
     <message>
-        <location filename="../qml/common/AboutModel.qml" line="11"></location>
+        <location filename="../qml/common/AboutModel.qml" line="11"/>
         <source>on GitHub</source>
         <translation>on GitHub</translation>
     </message>
     <message>
-        <location filename="../qml/common/AboutModel.qml" line="12"></location>
-        <location filename="../qml/common/AboutModel.qml" line="18"></location>
-        <location filename="../qml/common/AboutModel.qml" line="24"></location>
+        <location filename="../qml/common/AboutModel.qml" line="12"/>
+        <location filename="../qml/common/AboutModel.qml" line="18"/>
+        <location filename="../qml/common/AboutModel.qml" line="24"/>
         <source>Project</source>
         <translation>Project</translation>
     </message>
     <message>
-        <location filename="../qml/common/AboutModel.qml" line="16"></location>
+        <location filename="../qml/common/AboutModel.qml" line="16"/>
         <source>License</source>
         <translation>License</translation>
     </message>
     <message>
-        <location filename="../qml/common/AboutModel.qml" line="22"></location>
+        <location filename="../qml/common/AboutModel.qml" line="22"/>
         <source>Report a bug or send a feature request</source>
         <translation>Report a bug or send a feature request</translation>
     </message>
     <message>
-        <location filename="../qml/common/AboutModel.qml" line="23"></location>
+        <location filename="../qml/common/AboutModel.qml" line="23"/>
         <source>on GitHub Issues</source>
         <translation>on GitHub Issues</translation>
     </message>
     <message>
-        <location filename="../qml/common/AboutModel.qml" line="28"></location>
-        <location filename="../qml/common/AboutModel.qml" line="30"></location>
-        <location filename="../qml/common/AboutModel.qml" line="39"></location>
-        <location filename="../qml/common/AboutModel.qml" line="45"></location>
-        <location filename="../qml/common/AboutModel.qml" line="51"></location>
+        <location filename="../qml/common/AboutModel.qml" line="28"/>
+        <location filename="../qml/common/AboutModel.qml" line="30"/>
+        <location filename="../qml/common/AboutModel.qml" line="39"/>
+        <location filename="../qml/common/AboutModel.qml" line="45"/>
+        <location filename="../qml/common/AboutModel.qml" line="51"/>
         <source>Author</source>
         <translation>Author</translation>
     </message>
     <message>
-        <location filename="../qml/common/AboutModel.qml" line="37"></location>
+        <location filename="../qml/common/AboutModel.qml" line="37"/>
         <source>Twitter</source>
         <translation>Twitter</translation>
     </message>
     <message>
-        <location filename="../qml/common/AboutModel.qml" line="43"></location>
-        <location filename="../qml/common/AboutModel.qml" line="49"></location>
+        <location filename="../qml/common/AboutModel.qml" line="43"/>
+        <location filename="../qml/common/AboutModel.qml" line="49"/>
         <source>Donate</source>
         <translation>Donate</translation>
     </message>
     <message>
-        <location filename="../qml/common/AboutModel.qml" line="44"></location>
+        <location filename="../qml/common/AboutModel.qml" line="44"/>
         <source>via PayPal</source>
         <translation>via PayPal</translation>
     </message>
     <message>
-        <location filename="../qml/common/AboutModel.qml" line="50"></location>
+        <location filename="../qml/common/AboutModel.qml" line="50"/>
         <source>via Flattr</source>
         <translation>via Flattr</translation>
     </message>
     <message>
-        <location filename="../qml/common/AboutModel.qml" line="55"></location>
+        <location filename="../qml/common/AboutModel.qml" line="55"/>
         <source>Translate this app</source>
         <translation>Translate this app</translation>
     </message>
     <message>
-        <location filename="../qml/common/AboutModel.qml" line="56"></location>
+        <location filename="../qml/common/AboutModel.qml" line="56"/>
         <source>on translate.neufing.org</source>
         <translation>on translate.neufing.org</translation>
     </message>
     <message>
-        <location filename="../qml/common/AboutModel.qml" line="57"></location>
+        <location filename="../qml/common/AboutModel.qml" line="57"/>
         <source>Translation</source>
         <translation>Translation</translation>
     </message>
@@ -84,22 +84,22 @@
 <context>
     <name>AboutPage</name>
     <message>
-        <location filename="../qml/pages/AboutPage.qml" line="16"></location>
+        <location filename="../qml/pages/AboutPage.qml" line="16"/>
         <source>Settings</source>
         <translation>Settings</translation>
     </message>
     <message>
-        <location filename="../qml/pages/AboutPage.qml" line="26"></location>
+        <location filename="../qml/pages/AboutPage.qml" line="26"/>
         <source>About SailfishReboot</source>
         <translation>About SailfishReboot</translation>
     </message>
     <message>
-        <location filename="../qml/pages/AboutPage.qml" line="46"></location>
+        <location filename="../qml/pages/AboutPage.qml" line="46"/>
         <source>Link will open</source>
         <translation>Link will open</translation>
     </message>
     <message>
-        <location filename="../qml/pages/AboutPage.qml" line="49"></location>
+        <location filename="../qml/pages/AboutPage.qml" line="49"/>
         <source>Mail app will open</source>
         <translation>Mail app will open</translation>
     </message>
@@ -107,7 +107,7 @@
 <context>
     <name>CoverPage</name>
     <message>
-        <location filename="../qml/cover/CoverPage.qml" line="58"></location>
+        <location filename="../qml/cover/CoverPage.qml" line="58"/>
         <source>Uptime</source>
         <translation>Uptime</translation>
     </message>
@@ -115,7 +115,7 @@
 <context>
     <name>LicensePage</name>
     <message>
-        <location filename="../qml/pages/LicensePage.qml" line="21"></location>
+        <location filename="../qml/pages/LicensePage.qml" line="21"/>
         <source>License</source>
         <translation>License</translation>
     </message>
@@ -123,27 +123,27 @@
 <context>
     <name>MainPage</name>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="46"></location>
+        <location filename="../qml/pages/MainPage.qml" line="46"/>
         <source>Your device will shutdown</source>
         <translation>Your device will shutdown</translation>
     </message>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="52"></location>
+        <location filename="../qml/pages/MainPage.qml" line="52"/>
         <source>Your device will reboot</source>
         <translation>Your device will reboot</translation>
     </message>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="69"></location>
+        <location filename="../qml/pages/MainPage.qml" line="69"/>
         <source>Reboot</source>
         <translation>Reboot</translation>
     </message>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="89"></location>
+        <location filename="../qml/pages/MainPage.qml" line="89"/>
         <source>Shutdown</source>
         <translation>Shutdown</translation>
     </message>
     <message>
-        <location filename="../qml/pages/MainPage.qml" line="128"></location>
+        <location filename="../qml/pages/MainPage.qml" line="128"/>
         <source>Uptime</source>
         <translation>Uptime</translation>
     </message>
@@ -151,35 +151,37 @@
 <context>
     <name>SettingsPage</name>
     <message>
-        <location filename="../qml/pages/SettingsPage.qml" line="18"></location>
+        <location filename="../qml/pages/SettingsPage.qml" line="18"/>
         <source>Settings</source>
         <translation>Settings</translation>
     </message>
     <message>
-        <location filename="../qml/pages/SettingsPage.qml" line="22"></location>
+        <location filename="../qml/pages/SettingsPage.qml" line="22"/>
         <source>Custom colors</source>
         <translation>Custom colors</translation>
     </message>
     <message>
-        <location filename="../qml/pages/SettingsPage.qml" line="23"></location>
+        <location filename="../qml/pages/SettingsPage.qml" line="23"/>
         <source>Use custom colors for reboot and shutdown actions</source>
         <translation>Use custom colors for reboot and shutdown actions</translation>
     </message>
     <message>
-        <location filename="../qml/pages/SettingsPage.qml" line="32"></location>
+        <location filename="../qml/pages/SettingsPage.qml" line="32"/>
         <source>Remorse timeout</source>
         <translation>Remorse timeout</translation>
     </message>
     <message>
-        <location filename="../qml/pages/SettingsPage.qml" line="42"></location>
+        <location filename="../qml/pages/SettingsPage.qml" line="42"/>
         <source>Off</source>
         <translation>Off</translation>
     </message>
     <message numerus="yes">
-        <location filename="../qml/pages/SettingsPage.qml" line="44"></location>
+        <location filename="../qml/pages/SettingsPage.qml" line="44"/>
         <source>%n second(s)</source>
-        <translation><numerusform>%n second</numerusform>
-        <numerusform>%n seconds</numerusform>
-        </translation></message>
+        <translation>
+            <numerusform>%n second</numerusform>
+            <numerusform>%n seconds</numerusform>
+        </translation>
+    </message>
 </context>
 </TS>
